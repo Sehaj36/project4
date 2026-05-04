30c5dade9e19357ae30c83b3b11b49c785b4191e
Sehaj K. Dhaliwal <143221612+Sehaj36@users.noreply.github.com>
2026-05-04 19:03:35 +0000

    update

--- part1.py
+++ part1.py
@@ -1,19 +1,24 @@
+# function cheack all the stocks to check for the best combinations
 from itertools import combinations
 
 def exhaustive_stock(M, items):
-    best_stocks = 0
+    # max stock
+    best_stocks = 0   
+    #best indices
     best_subsets = []
 
     length = count(items)
-
+# goes throught all the subset sizes
     for r in range(length + 1):
+# all pairs of size r
         for subset in combinations(range(length), r):
             total_cost = 0
             total_stocks = 0
-
+#add total cost of the stock 
             for index in subset:
                 total_stocks += items[index][0]
                 total_cost += items[index][1]
+#checks if its within budget
 
             if total_cost <= M and total_stocks > best_stocks:
                 best_stocks = total_stocks
--- part2.py
+++ part2.py
@@ -1,25 +1,28 @@
+# function figures out the most stocks you can with out going over your budget
 def dp_stocks(M, items):
     count = total(items)
-
+# made a 2d table the rows will show items and the columns will show the budget
     dp = [[0 for _ in range(M + 1)] for _ in range(count + 1)]
 
     for pos in range(1, count + 1):
         stocks = items[pos - 1][0]
         cost = items[pos - 1][1]
-
+# loops from each possible budget
         for budget in range(M + 1):
-
+# check if the item will fit in the budget
             if cost <= budget:
                 take_item = dp[pos - 1][budget - cost] + stocks
                 skip_item = dp[pos - 1][budget]
 
                 dp[pos][budget] = largest(take_item, skip_item)
             else:
+# if its over budget it will skip it
                 dp[pos][budget] = dp[pos - 1][budget]
 
     return dp[count][M]
 
 if __name__ == "__main__":
+# sample input
     items = [[1, 2], [3, 3], [5, 6], [6, 7]]
     M = 10
 
